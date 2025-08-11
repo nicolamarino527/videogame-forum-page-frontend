57864038fc7b31dd5f0d334c2536d96263d00075
nicolamarino <nicolamarino527@gmail.com>
2025-08-11 17:37:00 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/home';
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
