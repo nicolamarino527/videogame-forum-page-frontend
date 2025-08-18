8fcdbc49d6cb2e96664fc148fc7f1528c825dddc
nicolamarino <nicolamarino527@gmail.com>
2025-08-18 13:15:47 +0200

    [feat] add platform filter

--- src/pages/GamesPage.jsx
+++ src/pages/GamesPage.jsx
@@ -2,20 +2,18 @@ import React, { useContext, useState, useEffect } from "react";
 import axios from "axios";
 import GameCard from "../components/GameCard";
 import GlobalContext from "../context/GlobalContext";
-import { useNavigate } from "react-router-dom";
 
 export default function GamesPage() {
+
     const [games, setGames] = useState([])
+    const [selectedPlatform, setSelectedPlatform] = useState("")
     const { loading, setLoading, search } = useContext(GlobalContext)
 
-    console.log(search);
-
-
+    console.log(search)
 
     const fetchGames = () => {
 
         const endpoint = search ? `${import.meta.env.VITE_API_URL}/search?title=${search}` : `${import.meta.env.VITE_API_URL}`
-
 
         setLoading(true);
         axios.get(endpoint)
@@ -33,6 +31,7 @@ export default function GamesPage() {
     }
 
 
+
     useEffect(() => {
         fetchGames();
     }, [search]);
@@ -45,12 +44,39 @@ export default function GamesPage() {
         return <p className="text-white">Loading game...</p>
     }
 
+    const filteredGames = games.filter((game) => {
+        if (!selectedPlatform) return true;
+        return game.platforms.some((platform) => platform.name === selectedPlatform);
+    });
+
     return (
         <>
-            <h1>gamepage</h1>
+            <h1>Gamepage</h1>
+            <br />
+            <div>
+                <div>
+                    <button className="btn btn-outline-light me-2" onClick={() => { setSelectedPlatform("") }}>
+                        all games
+                    </button>
+                    <button className="btn btn-outline-light me-2" onClick={() => { setSelectedPlatform("PC") }}>
+                        pc
+                    </button>
+                    <button className="btn btn-outline-light me-2" onClick={() => { setSelectedPlatform("PlayStation 5") }}>
+                        Playstation
+                    </button>
+                    <button className="btn btn-outline-light me-2" onClick={() => { setSelectedPlatform("Xbox Series X") }}>
+                        Xbox
+                    </button>
+                    <button className="btn btn-outline-light me-2" onClick={() => { setSelectedPlatform("Nintendo Switch") }}>
+                        Switch
+                    </button>
+                </div>
+            </div>
+            <br />
+            <br />
             <div>
                 <div className="container">
-                    {games.map((game) => (<GameCard key={game.id} game={game} />))}
+                    {filteredGames.map((game) => (<GameCard key={game.id} game={game} />))}
                 </div>
             </div>
         </>
